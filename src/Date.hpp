096a7946028c0999d50693e203ab8c14e8d3edea
kurrrru <kurrrru8850@gmail.com>
2025-08-30 17:07:26 +0900

    update Date

--- src/Date.hpp
+++ src/Date.hpp
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <iostream>
+#include <calender_system/CalenderSystem.hpp>
+
+namespace toolbox {
 
 class Date {
  public:
@@ -11,21 +14,22 @@ class Date {
     ~Date();
 
     explicit Date(int serial_date);
-    Date(int day, int month, int year);
-    Date(const std::string& date_str, const char* format = "%Y-%m-%d");
+    Date(CalendarSystem cal_sys, int day, int month, int year);
+    Date(CalendarSystem cal_sys, const std::string& date_str,
+            const char* format = "%Y-%m-%d");
 
     std::string to_string(const char* format = "%Y-%m-%d") const;
 
     int get_raw_date() const;
-    int get_day() const;
-    int get_month() const;
-    int get_year() const;
-    int get_weekday() const; // 0=Sun, 1=Mon, ..., 6=Sat
+    int get_day(CalendarSystem cal_sys) const;
+    int get_month(CalendarSystem cal_sys) const;
+    int get_year(CalendarSystem cal_sys) const;
+    int get_weekday(CalendarSystem cal_sys) const;  // 0=Sun, 1=Mon, ..., 6=Sat
 
-    Date& operator++(); // 前置インクリメント
-    Date operator++(int); // 後置インクリメント
-    Date& operator--(); // 前置デクリメント
-    Date operator--(int); // 後置デクリメント
+    Date& operator++();  // 前置インクリメント
+    Date operator++(int);  // 後置インクリメント
+    Date& operator--();  // 前置デクリメント
+    Date operator--(int);  // 後置デクリメント
 
     Date operator+(const int delta) const;
     Date operator-(const int delta) const;
@@ -34,5 +38,12 @@ class Date {
     Date& operator-=(const int delta);
 
  private:
-    int _serial_date; // 0 mean 1970-01-01 (Unix epoch)
+    void convert_form_serial_date(CalendarSystem cal_sys,
+            std::string& era, int& year, int& month, int& day) const;
+    int convert_to_serial_date(CalendarSystem cal_sys,
+            std::string era, int year, int month, int day) const;
+
+    int _serial_date;  // 0 mean 1970-01-01 (Unix epoch)
 };
+
+}  // namespace toolbox
---
+++ src/calender_system/CalenderSystem.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace toolbox {
+
+enum CalendarSystem {
+    GREGORIAN,
+    JULIAN,
+    JAPANESE_WAREKI,
+    FRENCH_REVOLUTIONARY
+};
+
+}  // namespace toolbox
